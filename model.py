277ae833a747e08dd7bfc5235f8c4134df80cc87
ShaliniJoanF <Franklinshalinijoan@gmail.com>
2025-09-10 16:15:37 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,13 +1,13 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 import pickle
 
-# Load dataset
-data = pd.read_csv("titanic.csv.csv")
 
-# Features
-X = data[["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare"]]
+data = pd.read_csv("/dev/titanic.csv")
+
+# Select features
+X = data[["Pclass", "Sex", "Age", "Fare"]]
 X["Sex"] = X["Sex"].map({"male": 0, "female": 1})
 X = X.fillna(X.mean())
 
@@ -16,14 +16,12 @@ y = data["Survived"]
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Train model
-model = LogisticRegression(max_iter=200)
+# Train Linear Regression
+model = LinearRegression()
 model.fit(X_train, y_train)
 
 # Save model
-with open("titanic_model.pkl", "wb") as f:
+with open("titanic_linreg.pkl", "wb") as f:
     pickle.dump(model, f)
 
-print("✅ Model trained and saved as titanic_model.pkl")
-
-
+print("✅ Linear Regression model trained and saved as titanic_linreg.pkl")
