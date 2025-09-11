fea3ce3720a83168d4a52aedff7c3cb6e9a1471e
ShaliniJoanF <Franklinshalinijoan@gmail.com>
2025-09-11 20:33:11 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,28 +1,35 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-import pickle
+from sklearn.metrics import mean_squared_error, r2_score
+
+import warnings
+warnings.filterwarnings('ignore')
+
+df = pd.read_csv('/titanic.csv')
 
 
-data = pd.read_csv("titanic.csv")
+x= df.drop(['Name','Sex','Ticket','Age','Cabin','Embarked'],axis=1)
+y=df['Survived']
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+print(x_train.shape)
+print(x_test.shape)
+print(y_train.shape)
+print(y_test.shape)
 
-# Select features
-X = data[["Pclass", "Sex", "Age", "Fare"]]
-X["Sex"] = X["Sex"].map({"male": 0, "female": 1})
-X = X.fillna(X.mean())
+model=LinearRegression()
+model.fit(x_train,y_train)
+y_pred=model.predict(x_test)
+print(y_pred)
 
-y = data["Survived"]
+concat=pd.concat([x_test,y_test],axis=1)
+print(concat)
 
-# Train-test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Train Linear Regression
-model = LinearRegression()
-model.fit(X_train, y_train)
-
-# Save model
-with open("titanic_linreg.pkl", "wb") as f:
-    pickle.dump(model, f)
-
-print("✅ Linear Regression model trained and saved as titanic_linreg.pkl")
-
+mse=mean_squared_error(y_pred,y_test)
+r2=r2_score(y_pred,y_test)
+print(mse)
+print(r2)
